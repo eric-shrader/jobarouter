58296ed17190647bb0aaa8b622fd0ab55ce1405e
eric-shrader <ericshrader101@gmail.com>
2023-08-04 13:02:56 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
+import {
+  createBrowserRouter,
+  Route,
+  createRoutesFromElements,
+  RouterProvider,
+} from 'react-router-dom'
+
+//pages
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Contact from './components/pages/help/Contact'
+import Faq from './components/pages/help/Faq'
+import NotFound from './components/pages/NotFound'
+import Careers, { careersLoader } from './components/pages/Careers'
+
+// layouts
+import RootLayout from './components/layouts/RootLayout'
+import HelpLayout from './components/layouts/HelpLayout'
+import CareersLayout from './components/layouts/CareersLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="help" element={<HelpLayout />}>
+        <Route path="faq" element={<Faq />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
+
+      <Route path="careers" element={<CareersLayout />}>
+        <Route index element={<Careers />} loader={careersLoader} />
+      </Route>
+
+      {/* route for when URL doesnt match a page */}
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
+
 function App() {
-  return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
-  );
+  return <RouterProvider router={router} />
 }
 
 export default App
---
+++ src/components/pages/Careers.js
@@ -0,0 +1,26 @@
+import { useLoaderData, Link } from 'react-router-dom'
+
+const Careers = () => {
+  let careers = []
+  careers = useLoaderData()
+
+  return (
+    <div className="careers">
+      {careers.map((career) => (
+        <Link to="/" key={career.id}>
+          <p>{career.title}</p>
+          <p>Based in {career.location}</p>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Careers
+
+// loader function
+export const careersLoader = async () => {
+  const res = await fetch('http://localhost:4000/careers')
+
+  return res.json()
+}
